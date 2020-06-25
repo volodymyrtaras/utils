a7a3d525b289164cd835fcbe189ee3abfca18a3a
vtaras <vtaras@varteq.com>
2020-06-25 11:55:27 +0300

    Refactor changes

---
+++ src/main/java/vtaras/own/utils/examples/java/linked/OwnSinglyLinkedList.java
@@ -0,0 +1,53 @@
+package vtaras.own.utils.examples.java.linked;
+
+public class OwnSinglyLinkedList {
+
+    public Node head;
+
+    public void append(Object data) {
+        if (head == null) {
+            head = new Node(data);
+            return;
+        }
+        tail().next = new Node(data);
+    }
+
+    public Node tail() {
+        Node tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        return tail;
+    }
+
+
+    public static class Node {
+        public Node next;
+        public String data;
+        public int intData;
+
+        public Node(Object o) {
+            if (o instanceof String) {
+                this.data = (String) o;
+            } else if (o instanceof Integer) {
+                this.intData = (int) o;
+            }
+        }
+
+        public Node(String data) {
+            this.data = data;
+        }
+
+        public int getIntData() {
+            return intData;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public Node(int intData) {
+            this.intData = intData;
+        }
+    }
+}
